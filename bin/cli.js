9f5f757c19c8ea6a45778f3ee3064c7b1854367e
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-21 23:34:49 +0530

    cli will be update according to the new feautes

--- bin/cli.js
+++ bin/cli.js
@@ -1,38 +1,67 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import inquirer from 'inquirer';
-import { getPublicIP, getLocalIP, getDNS, getISP } from '../src/networkInfo.js';
+import chalk from 'chalk';
+import figlet from 'figlet';
+import {
+  fetchPublicIP,
+  fetchLocalIP,
+  fetchDNSInfo,
+  fetchISPInfo,
+  pingWebsite,
+  speedTest
+} from '../src/networkInfo.js';
 
 const program = new Command();
 
-async function startCLI() {
-  const answers = await inquirer.prompt([
-    {
-      type: 'checkbox',
-      name: 'options',
-      message: 'Select the network details you want to fetch:',
-      choices: [
-        { name: 'Public IP', value: 'publicIP' },
-        { name: 'Local IP & Network Info', value: 'localIP' },
-        { name: 'DNS Information', value: 'dns' },
-        { name: 'ISP Details', value: 'isp' },
-      ],
-    },
-  ]);
+// Display Title
+console.log(chalk.blue(figlet.textSync('My Network', { horizontalLayout: 'full' })));
+console.log(chalk.gray('A simple network CLI tool'));
+console.log(chalk.yellow('-----------------------------------'));
 
-  console.log('\nFetching details...\n');
-
-  if (answers.options.includes('publicIP')) console.log(await getPublicIP());
-  if (answers.options.includes('localIP')) console.log(await getLocalIP());
-  if (answers.options.includes('dns')) console.log(await getDNS());
-  if (answers.options.includes('isp')) console.log(await getISP());
-}
-
-// Define CLI commands
+// Define Commands
 program
   .version('1.0.0')
-  .description('Network Info Fetcher CLI')
-  .action(startCLI);
+  .description('Network Utility CLI');
 
+// Command: Public IP
+program
+  .command('public-ip')
+  .description('Fetch Public IP')
+  .action(fetchPublicIP);
+
+// Command: Local IP
+program
+  .command('local-ip')
+  .description('Fetch Local IP')
+  .action(fetchLocalIP);
+
+// Command: DNS Info
+program
+  .command('dns')
+  .description('Fetch DNS Info')
+  .action(fetchDNSInfo);
+
+// Command: ISP Info
+program
+  .command('isp')
+  .description('Fetch ISP Details')
+  .action(fetchISPInfo);
+
+// Command: Ping
+program
+  .command('ping <website>')
+  .description('Ping a website (default: google.com)')
+  .action(pingWebsite);
+
+// Command: Speed Test
+program
+  .command('speedtest')
+  .description('Run Internet Speed Test')
+  .action(speedTest);
+
+// Show Credits
+program.addHelpText('after', chalk.magenta('\n💻 Dev by Sourav Chhimpa 🚀'));
+
+// Parse CLI args
 program.parse(process.argv);
